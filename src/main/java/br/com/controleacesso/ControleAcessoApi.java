17e6f840a71f0c03a4450f764e8ca162d7d240a7
Natan Gomes <gomesnatan84@gmail.com>
2025-11-23 20:22:27 -0300

    alterando a versão do java para 17

--- src/main/java/br/com/controleacesso/ControleAcessoApi.java
+++ src/main/java/br/com/controleacesso/ControleAcessoApi.java
@@ -1,16 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package br.com.controleacesso;
 
-/**
- *
- * @author Natan Vieira
- */
 public class ControleAcessoApi {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+    
 }
